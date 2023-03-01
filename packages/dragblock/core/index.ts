ef9e80a4c8cac12692415001a2e3414ef853d9e6
pinky-pig <2545589993@qq.com>
2023-03-01 17:18:16 +0800

    feat: v3-dragblock 改成了 dragblock

---
+++ packages/dragblock/core/index.ts
@@ -0,0 +1,2 @@
+import V3Dragblock from './GridContainer.vue'
+export default V3Dragblock as typeof V3Dragblock
---
+++ packages/dragblock/vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), dts()],
+  server: {
+    host: '0.0.0.0',
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'core/index.ts'),
+      name: 'v3-dragblock',
+      fileName: format => `v3-dragblock.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+
+})
