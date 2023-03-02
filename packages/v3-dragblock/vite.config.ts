169679279bd3182e44fdb90ee70d5a4527e6c85a
pinky-pig <2545589993@qq.com>
2023-03-02 15:37:15 +0800

    chore: eslint code

--- packages/v3-dragblock/vite.config.ts
+++ packages/v3-dragblock/vite.config.ts
@@ -2,9 +2,13 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [
+    vue(),
+    dts(),
+  ],
   server: {
     host: '0.0.0.0',
   },
@@ -22,6 +26,7 @@ export default defineConfig({
         },
       },
     },
+
   },
 
 })
