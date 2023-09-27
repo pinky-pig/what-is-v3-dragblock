9a27bae9c09d4f78eb05ba56aeb4caf3ba31ab87
pinky-pig <2545589993@qq.com>
2023-09-27 17:45:06 +0800

    fix: demo styles

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -42,8 +42,9 @@ const print = (val: string, e: any) => {
     @resize-end="print('resize-end', $event)"
   />
   <footer class="footer">
-    <a target="_blank" href="https://github.com/pinky-pig/what-is-v3-dragblock" class="item"> GitHub </a>
-    <a target="_blank" href="https://mmeme.me/" class="item"> Demo </a>
+    <a target="_blank" href="https://github.com/pinky-pig/what-is-v3-dragblock" class="item">
+      <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 32 32"><path fill="currentColor" fill-rule="evenodd" d="M16 2a14 14 0 0 0-4.43 27.28c.7.13 1-.3 1-.67v-2.38c-3.89.84-4.71-1.88-4.71-1.88a3.71 3.71 0 0 0-1.62-2.05c-1.27-.86.1-.85.1-.85a2.94 2.94 0 0 1 2.14 1.45a3 3 0 0 0 4.08 1.16a2.93 2.93 0 0 1 .88-1.87c-3.1-.36-6.37-1.56-6.37-6.92a5.4 5.4 0 0 1 1.44-3.76a5 5 0 0 1 .14-3.7s1.17-.38 3.85 1.43a13.3 13.3 0 0 1 7 0c2.67-1.81 3.84-1.43 3.84-1.43a5 5 0 0 1 .14 3.7a5.4 5.4 0 0 1 1.44 3.76c0 5.38-3.27 6.56-6.39 6.91a3.33 3.33 0 0 1 .95 2.59v3.84c0 .46.25.81 1 .67A14 14 0 0 0 16 2Z" /></svg>
+    </a>
   </footer>
 </template>
 
@@ -60,27 +61,17 @@ const print = (val: string, e: any) => {
   box-shadow: 5px 5px 41px 5px rgba(0,0,0,.45);
 }
 .footer{
+  position: fixed;
+  bottom: 0;
+  left: 0;
   width: 100%;
-  height: 50px;
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: start;
   align-items: center;
-  margin-top: 15px;
 }
 .item{
-  margin: 0 10px;
-  color: #474747;
-  text-decoration: none;
-  margin: 10px 10px;
-  font-weight: 800;
-  font-size: 20px;
   cursor: pointer;
   padding: 10px;
-}
-.item:hover{
-  color: #f7f4f0;
-  background: #474747;
-  border-radius: 5px;
 }
 </style>
